ed3591e6ac1a662e2e015a60ea63fe452eb6253a
Gunhild <afsoon.alizadeh1991@gmail.com>
2024-01-04 00:43:15 +0330

    create select component

---
+++ src/app/core/component/select/select.component.ts
@@ -0,0 +1,36 @@
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.sass'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class SelectComponent {
+  @Input() noFocus: boolean = false;
+  @Input() spinner: boolean = false;
+
+  constructor(private el: ElementRef<HTMLElement>) {}
+
+  @HostBinding('attr.spinning')
+  public get spinning() {
+    return this.spinner;
+  }
+
+  @HostListener('keyup.enter')
+  onEnter() {
+    this.el.nativeElement.click();
+  }
+
+  @HostListener('click')
+  onClick() {
+    if (this.spinner) return;
+  }
+}
